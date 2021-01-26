aef69e2686e887e50acedcb26d96c152348c88ce
Renan Silva <renanh2008@hotmail.com>
2021-01-26 01:45:34 -0300

    Colocando os inputs fields

--- app/src/main/java/digitalhouse/desafio/desafiofirebase/Login.kt
+++ app/src/main/java/digitalhouse/desafio/desafiofirebase/Login.kt
@@ -1,11 +1,25 @@
 package digitalhouse.desafio.desafiofirebase
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        findViewById<Button>(R.id.bt_login).setOnClickListener(openActivity(Home::class.java))
+        findViewById<TextView>(R.id.txt_register).setOnClickListener(openActivity(Register::class.java))
+    }
+
+    fun openActivity(activity: Class<out Activity>): View.OnClickListener? {
+        return View.OnClickListener {
+            startActivity(Intent(this, activity))
+        }
     }
 }
