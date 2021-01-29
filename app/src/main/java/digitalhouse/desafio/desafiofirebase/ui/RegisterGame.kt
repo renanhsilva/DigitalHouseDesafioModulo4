9922c7ea5c2caa56e1b88d6189bd4110427d3786
Renan Silva <renanh2008@hotmail.com>
2021-01-29 10:04:10 -0300

    Cadastrando o jogo no firebase

--- app/src/main/java/digitalhouse/desafio/desafiofirebase/ui/RegisterGame.kt
+++ app/src/main/java/digitalhouse/desafio/desafiofirebase/ui/RegisterGame.kt
@@ -11,6 +11,7 @@ import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import com.squareup.picasso.Picasso
 import digitalhouse.desafio.desafiofirebase.databinding.ActivityRegisterGameBinding
+import digitalhouse.desafio.desafiofirebase.service.cr
 import dmax.dialog.SpotsDialog
 import kotlinx.android.synthetic.main.activity_register_game.*
 
@@ -22,19 +23,26 @@ class RegisterGame : AppCompatActivity() {
     lateinit var storageReference: StorageReference
     private val CODE_IMG = 1000
 
+    lateinit var urlimagem: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterGameBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        binding.imgFoto.setOnClickListener {
-            loadimg()
+        binding.imgLoadGame.setOnClickListener {
+            loading()
+        }
+
+        binding.inRegisterGamer.btCreateRegisterGame.setOnClickListener {
+            var dataGame = data()
+            uploadGame(dataGame)
         }
 
     }
 
-    private fun loadimg() {
+    private fun loading() {
         val intent = Intent()
         intent.type = "image/"
         intent.action = Intent.ACTION_GET_CONTENT
@@ -44,7 +52,7 @@ class RegisterGame : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         alertDialog = SpotsDialog.Builder().setContext(this).build()
-        storageReference = FirebaseStorage.getInstance().getReference("prod_img")
+        storageReference = FirebaseStorage.getInstance().getReference(data.toString())
         if (requestCode == CODE_IMG) {
             alertDialog.show()
             val uploadTask = storageReference.putFile(data!!.data!!)
@@ -56,14 +64,38 @@ class RegisterGame : AppCompatActivity() {
             }.addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     val downloadUri = task.result
-                    val url = downloadUri!!.toString()
-                            .substring(0, downloadUri.toString().indexOf("&token"))
+                    val url = downloadUri!!.toString().substring(0, downloadUri.toString().indexOf("&token"))
                     Log.i("Link Direto ", url)
                     alertDialog.dismiss()
                     Picasso.get().load(url).into(img_load_game)
+                    urlimagem = url
                 }
             }
         }
 
     }
+
+    fun data(): MutableMap<String, Any>{
+        val game: MutableMap<String, Any> = HashMap()
+
+        game["name"] = binding.inRegisterGamer.txtNameRegisterGame.text.toString()
+        game["year"] = binding.inRegisterGamer.txtYearRegisterGame.text.toString()
+        game["desc"] = binding.inRegisterGamer.txtDescRegisterGame.text.toString()
+        game["url"] = urlimagem
+
+        return game
+    }
+
+    fun uploadGame(jogo: MutableMap<String, Any>){
+        val nome = binding.inRegisterGamer.txtNameRegisterGame.text.toString()
+        cr.document(nome).set(jogo).addOnSuccessListener {
+            Log.i("UPLOADGAME","Envio com sucesso")
+            Toast.makeText(this, "Upload com sucesso!", Toast.LENGTH_SHORT).show()
+            var intent = Intent(application, Home::class.java)
+            startActivity(intent)
+        }.addOnFailureListener{
+            Log.i("UPLOADGAME", it.toString())
+        }
+    }
+
 }
